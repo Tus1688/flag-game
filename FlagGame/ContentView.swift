e82274267e3277cfd6f7b6c8d2e668823fdb17bf
Tus1688 <76937231+Tus1688@users.noreply.github.com>
2023-10-18 14:05:44 +0700

    feat(contentView): UI

--- FlagGame/ContentView.swift
+++ FlagGame/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    var asean = ["Indonesia", "Singapore", "Malaysia", "Laos", "Philipines", "Cambodia", "Myanmar", "Thailand", "Brunei", "Vietnam"]
+    var angkaRandom = Int.random(in: 0...9)
+//    var alreadyCorrect: [Int]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color.teal
+                .cornerRadius(24)
+                .shadow(radius: 12)
+                .ignoresSafeArea()
+            VStack{
+                Text("Pilih Bendera dari Negara : ")
+                    .foregroundStyle(.black)
+                Text(asean[angkaRandom])
+                    .bold()
+                    .font(.title)
+                    .foregroundStyle(.black)
+            }
         }
-        .padding()
+        .padding(.bottom)
+        
+        HStack{
+            Spacer()
+            VStack{
+                ForEach(0..<5) { number in
+                    Button {
+                    } label: {
+                        Image(asean[number])
+                            .resizable()
+                            .frame(width:105,height: 65)
+                            .cornerRadius(8)
+                            .shadow(radius: 8)
+                    }
+                }
+            }
+            Spacer()
+            VStack{
+                ForEach(5..<10) { number in
+                    Button {
+                    } label: {
+                        Image(asean[number])
+                            .resizable()
+                            .frame(width:105,height: 65)
+                            .cornerRadius(8)
+                            .shadow(radius: 8)
+                    }
+                }
+            }
+            Spacer()
+        }
     }
 }
 
